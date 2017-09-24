93dc157d239d98dc02023d205e347b0e841c684b
Chandra Sekhar <sekhar72@yahoo.com>
2017-09-24 13:31:45 +0530

    Testing Report

--- src/main/java/com/mycompany/demo/Myapp/App.java
+++ src/main/java/com/mycompany/demo/Myapp/App.java
@@ -4,10 +4,37 @@ package com.mycompany.demo.Myapp;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App { 
+ private String greeting ;
+ 
+ public String getGreeting() {
+ 	return this.greeting;
 }
+
+public void setGreeting(String greet) {
+	if (isGreetingValid(greet)) {
+		this.greeting = greet;
+	}
+}
+
+public boolean isGreetingValid(String greeting) {
+	if (greeting.contains("Welcome")
+			|| greeting.contains("thanks")
+			|| greeting.contains("good afternoon")) {
+		 return true;
+		 
+	}
+	
+	return false;
+	}
+
+}
+
+
+/**
+*    public static void main( String[] args )
+   * {
+       * System.out.println( "Hello World!" );
+    *}
+*}
+*/
--- src/test/java/com/mycompany/demo/Myapp/AppTest.java
+++ src/test/java/com/mycompany/demo/Myapp/AppTest.java
@@ -1,38 +1,16 @@
 package com.mycompany.demo.Myapp;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import junit.framework.Assert;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+public class AppTest {
+	App app = new App();
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Test
+	public void validateSetAndGet() {
+		app.setGreeting("good afternoon");
+		Assert.assertEquals("good afternoon", app.getGreeting());
+
+	}
 }
